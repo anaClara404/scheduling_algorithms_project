a96bc21d9a9ec27e8870aeeb3ef6b3a03cdc9899
Ana Clara <ana.clara18@academico.ufpb.br>
2025-04-29 16:54:11 -0300

    update FCFS.cpp
    add TurnaroundTime, WaitingTume and ResponseTime + AVG

--- FCFS.cpp
+++ FCFS.cpp
@@ -1,27 +1,50 @@
 #include "FCFS.h"
 #include <iostream>
 
-FCFS::FCFS(const std::list<Task>& tasks) : taskList(tasks) {}
-
 void FCFS::schedule() {
+    int currentTime = 0;
+    int finalTurnaroundTime = 0;
+    int finalWaitingTime = 0;
+    int finalResponseTime = 0;
+    int taskAmount = taskList.size();
+
     while (!taskList.empty()) {
-        Task task = pickNextTask();
+        Task currentTask = pickNextTask();
 
-        std::cout << "Executando: " << task.name
-                  << " [Burst: " << task.burst << "]" << std::endl;
-
-        if (!task.started) {
-            task.startTime = 0;  
-            task.started = true;
+        // Define tempo de início
+        if (!currentTask.started) {
+            currentTask.startTime = currentTime;
+            currentTask.started = true;
         }
 
-        // Simula execução 
-        task.endTime = task.startTime + task.burst;
+        // Simula execução até o fim
+        currentTask.endTime = currentTask.startTime + currentTask.burst;
+        currentTime = currentTask.endTime;
 
-        std::cout << "Finalizado: " << task.name
-                  << " [Tempo de fim: " << task.endTime << "]" << std::endl;
+        // Calcula métricas do processo
+        int taskTurnaroundTime = currentTask.endTime - currentTask.arrivalTime;
+        int taskWaitingTime = taskTurnaroundTime - currentTask.burst;
+        int taskResponseTime = currentTask.startTime - currentTask.arrivalTime;
+
+        // Acumula para média
+        finalTurnaroundTime += taskTurnaroundTime;
+        finalWaitingTime += taskWaitingTime;
+        finalResponseTime += taskResponseTime;
+
+        // Exibe resultados por processo
+        std::cout << "Processo [" << currentTask.name << "] finalizou sua execução.\n"
+                  << "Tempo de turnaround: " << taskTurnaroundTime << "\n"
+                  << "Tempo de espera: " << taskWaitingTime << "\n"
+                  << "Tempo de resposta: " << taskResponseTime << "\n\n";
     }
+
+    // Exibe médias
+    std::cout << "===== Todos os processos foram finalizados. =====\n";
+    std::cout << "-> Tempo médio de turnaround: " << (float)finalTurnaroundTime / taskAmount << "\n";
+    std::cout << "-> Tempo médio de espera: " << (float)finalWaitingTime / taskAmount << "\n";
+    std::cout << "-> Tempo médio de resposta: " << (float)finalResponseTime / taskAmount << "\n";
 }
+
 
 Task FCFS::pickNextTask() {
     Task nextTask = taskList.front();
